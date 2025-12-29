255a9db3f0ecd0349d998568902e8e85209a5554
DaniusKalv <danius@kurokesu.com>
2025-12-29 12:53:26 +0200

    Fine-tune exposure range

--- ar0234.c
+++ ar0234.c
@@ -58,10 +58,8 @@
 
 /* Exposure control */
 #define AR0234_REG_EXPOSURE_COARSE	0x3012
-#define AR0234_REG_EXPOSURE_FINE	0x3014
-#define AR0234_EXPOSURE_MIN		4
+#define AR0234_EXPOSURE_MIN			2
 #define AR0234_EXPOSURE_STEP		1
-#define AR0234_EXPOSURE_DEFAULT		0x640
 
 /* Analog gain control */
 #define AR0234_REG_ANALOG_GAIN	0x3060
@@ -565,15 +563,12 @@ static int ar0234_open(struct v4l2_subdev *sd, struct v4l2_subdev_fh *fh)
 
 static void ar0234_adjust_exposure_range(struct ar0234 *ar0234)
 {
-	int exposure_max, exposure_def;
+	int exposure_max = ar0234->mode.format->height + ar0234->vblank->val -
+						AR0234_EXPOSURE_MIN;
 
-	/* Honour the VBLANK limits when setting exposure. */
-	exposure_max = ar0234->mode.format->height + ar0234->vblank->val -
-		       AR0234_EXPOSURE_MIN;
-	exposure_def = min(exposure_max, ar0234->exposure->val);
 	__v4l2_ctrl_modify_range(ar0234->exposure, ar0234->exposure->minimum,
 				 exposure_max, ar0234->exposure->step,
-				 exposure_def);
+				 exposure_max);
 }
 
 static int ar0234_set_ctrl(struct v4l2_ctrl *ctrl)
