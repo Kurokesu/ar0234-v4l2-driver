f62a0af0bb0c20dbc057f80f5a2cb1482a645dd5
DaniusKalv <danius@kurokesu.com>
2026-05-05 18:18:13 +0300

    Add explicit include for linux/of.h

--- ar0234.c
+++ ar0234.c
@@ -14,6 +14,7 @@
 #include <linux/delay.h>
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/pm_runtime.h>
 #include <linux/regulator/consumer.h>
 
